aff9f4f682f8cdca1a2ec249c950f06392582b36
dnlx77 <dnlx77@gmail.com>
2020-06-15 17:11:56 +0200

    Window class

--- DirectxProject/Main.cpp
+++ DirectxProject/Main.cpp
@@ -1,5 +1,23 @@
 #include <Windows.h>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	const auto pClassName = "Prova";
+	WNDCLASSEX wc = { 0 };
+	
+	wc.cbClsExtra = 0;
+	wc.cbSize = sizeof(wc);
+	wc.lpfnWndProc = DefWindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIcon = nullptr;
+	wc.style = CS_OWNDC;
+	wc.cbWndExtra = 0;
+	wc.hbrBackground = nullptr;
+
+	RegisterClassEx(&wc);
+
+
+
 	return 0;
 }
